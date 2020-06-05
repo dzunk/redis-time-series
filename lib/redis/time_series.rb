f6c0983dfdf66d1cf669843166b22e08e5cb4448
Matt Duszynski <mduszynski@weedmaps.com>
2020-06-04 21:54:58 -0700

    Basic stub for Redis::TimeSeries class

--- lib/redis/time_series.rb
+++ lib/redis/time_series.rb
@@ -1,6 +1,18 @@
-require 'redis'
-
 class Redis
   class TimeSeries
+    class << self
+      def create(key, **options)
+        # TODO: TS.CREATE
+        new(key, **options)
+      end
+
+    end
+
+    attr_reader :redis
+
+    def initialize(key, options = {})
+      @key = key
+      @redis = options[:redis] || Redis.current
+    end
   end
 end
